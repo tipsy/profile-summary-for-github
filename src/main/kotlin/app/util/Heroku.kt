6805f02ce8e5faa32a292c9923ed7ad88d6aaf5e
David <davidaase@hotmail.com>
2017-12-14 00:31:35 +0100

    add comments to heroku-util

--- src/main/kotlin/app/util/Heroku.kt
+++ src/main/kotlin/app/util/Heroku.kt
@@ -4,15 +4,17 @@ import io.javalin.Javalin
 
 object Heroku {
 
+    // Get port from Heroku, or return null (localhost)
     fun getPort() = ProcessBuilder().environment()["PORT"]?.let {
         Integer.parseInt(it)
     }
 
+    // Get oauth-token from Heroku, or return null (localhost)
     fun getOauthToken() = ProcessBuilder().environment()["OAUTH_TOKEN"]
 
+    // Force HTTPS for all requests
     fun enableSslRedirect(app: Javalin) {
-        // This really should be solved in Heroku itself, but
-        // I'm incapable of finding any setting for it
+        // This really should be solved in Heroku itself, but I'm incapable of finding any setting for it
         app.before { ctx ->
             if (ctx.header("x-forwarded-proto") == "http") {
                 ctx.redirect("https://${ctx.header("host")}${ctx.path()}")
