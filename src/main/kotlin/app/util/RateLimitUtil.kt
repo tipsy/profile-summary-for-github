013de4a70394971b480acf01a2f1f15877b77ff6
David <davidaase@hotmail.com>
2018-01-10 19:22:58 +0100

    refactor RateLimitUtil a bit
    - inline runnable (similar to ghservice)
    - use trailing closures where possible

--- src/main/kotlin/app/util/RateLimitUtil.kt
+++ src/main/kotlin/app/util/RateLimitUtil.kt
@@ -22,28 +22,25 @@ object RateLimitUtil {
     fun enableTerribleRateLimiting(app: Javalin) {
 
         app.before { ctx ->
-            ipReqCount.compute(ctx.ip(), { _, count ->
+            ipReqCount.compute(ctx.ip()) { _, count ->
                 when (count) {
                     null -> 1
                     in 0..25 -> count + 1
                     else -> throw TerribleRateLimitException()
                 }
-            })
+            }
         }
 
         app.exception(TerribleRateLimitException::class.java) { _, ctx ->
             ctx.result("You can't spam this much. I'll give you a new request every five seconds.")
         }
 
-        Executors.newSingleThreadScheduledExecutor()
-                .scheduleAtFixedRate(decrementAllCounters, 0, 5, TimeUnit.SECONDS)
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate({
+            ipReqCount.forEachKey(1) { ip ->
+                ipReqCount.computeIfPresent(ip) { _, count -> if (count > 1) count - 1 else null }
+            }
+        }, 0, 5, TimeUnit.SECONDS)
 
-    }
-
-    private val decrementAllCounters = Runnable {
-        ipReqCount.forEachKey(1, { ip ->
-            ipReqCount.computeIfPresent(ip, { _, count -> if (count > 1) count - 1 else null })
-        })
     }
 
 }
