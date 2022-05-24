afd125373b6f07887ff7816263172fd20f9c9026
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-05-24 12:16:06 +0900

    Update src/main/kotlin/app/util/HikariCpDataSource.kt
    Co-authored-by: Sidd <iProdigy@users.noreply.github.com>

--- src/main/kotlin/app/util/HikariCpDataSource.kt
+++ src/main/kotlin/app/util/HikariCpDataSource.kt
@@ -7,16 +7,14 @@ import java.sql.Connection
 object HikariCpDataSource {
     private const val urlToDb = "jdbc:h2:mem:userinfo"
 
-    private val config = HikariConfig()
-    private var hikariDataSource: HikariDataSource?
-
-    init {
-        config.jdbcUrl = urlToDb
-        config.addDataSourceProperty("cachePrepStmts", "true")
-        config.addDataSourceProperty("prepStmtCacheSize", "250")
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-        hikariDataSource = HikariDataSource(config)
+    private val config = HikariConfig().apply {
+        jdbcUrl = urlToDb
+        addDataSourceProperty("cachePrepStmts", "true")
+        addDataSourceProperty("prepStmtCacheSize", "250")
+        addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
     }
 
-    val connection: Connection get() = hikariDataSource!!.connection
+    private var hikariDataSource = HikariDataSource(config)
+
+    val connection: Connection get() = hikariDataSource.connection
 }
