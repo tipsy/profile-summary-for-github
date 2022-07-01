66aad599efeeceedd25c348753ce8181316ab56c
iProdigy <iProdigy@users.noreply.github.com>
2022-06-30 21:37:03 -0500

    fix: apply h2 params in url

--- src/main/kotlin/app/util/HikariCpDataSource.kt
+++ src/main/kotlin/app/util/HikariCpDataSource.kt
@@ -5,13 +5,10 @@ import com.zaxxer.hikari.HikariDataSource
 import java.sql.Connection
 
 object HikariCpDataSource {
-    private const val urlToDb = "jdbc:h2:mem:userinfo"
+    private const val urlToDb = "jdbc:h2:mem:userinfo;DB_CLOSE_DELAY=-1;QUERY_CACHE_SIZE=256"
 
     private val config = HikariConfig().apply {
         jdbcUrl = urlToDb
-        addDataSourceProperty("cachePrepStmts", "true")
-        addDataSourceProperty("prepStmtCacheSize", "250")
-        addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
     }
 
     private var hikariDataSource = HikariDataSource(config)
